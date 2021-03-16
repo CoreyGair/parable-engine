d8028a8fde9e411c54c06143464a62fded52ce4a
yeroCG <corey_gair@yahoo.co.uk>
2021-03-16 00:06:12 +0000

    Reverting some incorrectly included files

--- .ccls-cache/@home@corey@Documents@parable-engine/Parable@src@Parable.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Included in applications using parable engine
-
-#include "Core/Application.h"
--- .ccls-cache/@home@corey@Documents@parable-engine/Parable@src@pblpch.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Precompiled header for parable engine
-
-#include <string>
-#include <iostream>
